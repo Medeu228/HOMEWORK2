433091ff8703cce15a8b6cce88717ebc4a0d4848
Medeu228 <iakubovmedeu@gmail.com>
2026-02-10 18:06:23 +0500

    fix

--- src/main/java/edu/narxoz/RPG/Main.java
+++ src/main/java/edu/narxoz/RPG/Main.java
@@ -1,34 +1,46 @@
 package edu.narxoz.RPG;
 
+import java.util.Scanner;
+
 import edu.narxoz.RPG.characters.Character;
 import edu.narxoz.RPG.factory_method.*;
 import edu.narxoz.RPG.abstract_factory.*;
+import edu.narxoz.RPG.factory_method.EquipmentFactory;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
-        CharacterFactory warriorFactory = new WarriorFactory();
-        CharacterFactory mageFactory = new MageFactory();
-        CharacterFactory archerFactory = new ArcherFactory();
+        // === Выбор персонажа (Factory Method)
+        System.out.println("Choose character: 1-Warrior, 2-Mage, 3-Archer");
+        int c = sc.nextInt();
 
-        Character warrior = warriorFactory.create();
-        Character mage = mageFactory.create();
-        Character archer = archerFactory.create();
+        CharacterFactory characterFactory;
 
+        if (c == 1) characterFactory = new WarriorFactory();
+        else if (c == 2) characterFactory = new MageFactory();
+        else characterFactory = new ArcherFactory();
 
-        EquipmentFactory medieval = new MedievalEquipmentFactory();
-        EquipmentFactory magic = new MagicEquipmentFactory();
-        EquipmentFactory ranger = new RangerEquipmentFactory();
+        Character character = characterFactory.create();
 
-        warrior.equip(medieval.createWeapon(), medieval.createArmor());
-        mage.equip(magic.createWeapon(), magic.createArmor());
-        archer.equip(ranger.createWeapon(), ranger.createArmor());
+        // === Выбор экипировки (Abstract Factory)
+        System.out.println("Choose equipment: 1-Medieval, 2-Magic, 3-Ranger");
+        int e = sc.nextInt();
 
+        EquipmentFactory equipmentFactory;
 
-        warrior.showInfo();
-        mage.showInfo();
-        archer.showInfo();
+        if (e == 1) equipmentFactory = new MedievalEquipmentFactory();
+        else if (e == 2) equipmentFactory = new MagicEquipmentFactory();
+        else equipmentFactory = new RangerEquipmentFactory();
+
+        character.equip(
+                equipmentFactory.createWeapon(),
+                equipmentFactory.createArmor()
+        );
+
+        // === Результат
+        character.showInfo();
     }
 }
